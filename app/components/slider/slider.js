4bb58832aff1f23f4c34c3304ada64560c5f5379
nick_437 <nick_437@mail.ru>
2017-06-02 02:27:10 +0200

    index + mob.version

---
+++ app/components/slider/slider.js
@@ -0,0 +1,12 @@
+import Swiper from 'swiper';
+
+export default () => {
+  // eslint-disable-next-line no-unused-vars
+  const mainSlider = new Swiper('.main-slider', {
+    pagination: '.main-slider__swiper-pagination',
+    paginationClickable: true,
+    speed: 400,
+    autoplay: 5000,
+    initialSlide: 1,
+  });
+};
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,8 @@
 import $ from 'jquery';
+import mainSlider from '../components/slider/slider';
+
 
 $(() => {
   // your code goes here
+  mainSlider();
 });
