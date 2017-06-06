6571f39f1141b92872a471cebcc079fb1983b45b
nick_437 <nick_437@mail.ru>
2017-06-06 11:38:50 +0300

    edits ie  + svg + navbar(link line)

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,10 @@
 import $ from 'jquery';
+import svg4everybody from 'svg4everybody';
 import mainSlider from '../components/slider/slider';
 
 
 $(() => {
   // your code goes here
   mainSlider();
+  svg4everybody();
 });
