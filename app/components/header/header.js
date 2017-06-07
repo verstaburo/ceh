ca7451ab949e9a575e109f724677b1ff66d5a0e1
nick_437 <nick_437@mail.ru>
2017-06-07 17:49:06 +0300

    edits full

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -6,25 +6,23 @@ export default () => {
   const visible = 'header__navbar_visible';
   const open = 'header__burger_open';
 
-  // Freeze page content scrolling
+// Freeze page content scrolling
   function freeze() {
-    let top;
-
-    if ($('html').css('position') !== 'fixed') {
-      top = $('html').scrollTop() ? $('html').scrollTop() : $('body').scrollTop();
-      if (window.innerWidth > $('html').width()) {
-        $('html').css('overflow-y', 'scroll');
+    if($("html").css("position") != "fixed") {
+      var top = $("html").scrollTop() ? $("html").scrollTop() : $("body").scrollTop();
+      if(window.innerWidth > $("html").width()) {
+        $("html").css("overflow-y", "scroll");
       }
-      $('html').css({ width: '100%', height: '100%', position: 'fixed', top: -top });
+      $("html").css({"width": "100%", "height": "100%", "position": "fixed", "top": -top});
     }
   }
 
 // Unfreeze page content scrolling
   function unfreeze() {
-    if ($('html').css('position') === 'fixed') {
-      $('html').css('position', 'static');
-      $('html, body').scrollTop(-parseInt($('html').css('top')));
-      $('html').css({ position: '', width: '', height: '', top: '', 'overflow-y': '' });
+    if($("html").css("position") == "fixed") {
+      $("html").css("position", "static");
+      $("html, body").scrollTop(-parseInt($("html").css("top")));
+      $("html").css({"position": "", "width": "", "height": "", "top": "", "overflow-y": ""});
     }
   }
 
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import 'babel-polyfill';
 import svg4everybody from 'svg4everybody';
 import objectFitImages from 'object-fit-images';
 import mainSlider from '../components/slider/slider';
