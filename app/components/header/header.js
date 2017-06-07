25dbe923fb11b330206849e337fa04dd5f30492d
nick_437 <nick_437@mail.ru>
2017-06-07 16:46:25 +0300

    edits

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -6,8 +6,35 @@ export default () => {
   const visible = 'header__navbar_visible';
   const open = 'header__burger_open';
 
+  // Freeze page content scrolling
+  function freeze() {
+    let top;
+
+    if ($('html').css('position') !== 'fixed') {
+      top = $('html').scrollTop() ? $('html').scrollTop() : $('body').scrollTop();
+      if (window.innerWidth > $('html').width()) {
+        $('html').css('overflow-y', 'scroll');
+      }
+      $('html').css({ width: '100%', height: '100%', position: 'fixed', top: -top });
+    }
+  }
+
+// Unfreeze page content scrolling
+  function unfreeze() {
+    if ($('html').css('position') === 'fixed') {
+      $('html').css('position', 'static');
+      $('html, body').scrollTop(-parseInt($('html').css('top')));
+      $('html').css({ position: '', width: '', height: '', top: '', 'overflow-y': '' });
+    }
+  }
+
   burger.on('click', () => {
     burger.toggleClass(open);
     navbar.toggleClass(visible);
+    if (burger.hasClass(open)) {
+      freeze();
+    } else {
+      unfreeze();
+    }
   });
 };
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,13 @@
 import $ from 'jquery';
 import svg4everybody from 'svg4everybody';
+import objectFitImages from 'object-fit-images';
 import mainSlider from '../components/slider/slider';
 import header from '../components/header/header';
 
 
 $(() => {
   // your code goes here
+  objectFitImages('.image-bg__img');
   mainSlider();
   svg4everybody();
   header();
